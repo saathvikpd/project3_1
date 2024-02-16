8c22287e89c715b03c6bcc09ea746e5760b78160
saathvikpd <saathvikpd@users.noreply.github.com>
2024-02-15 21:22:44 -0800

    Create App.svelte

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Graph from "./Graph.svelte";
+</script>
+<section class="container">
+    <svelte:component this={Graph}/></section>
+<style>
+    section {
+        width: 400px
+    }
+
+
+    .container {
+    max-width: 1000px;
+    border: solid 0px black;
+    margin: auto;
+    }
+
+</style>
